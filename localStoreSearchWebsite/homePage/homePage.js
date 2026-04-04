4d7ea4dc19f1456bb73a6bc115c427abc1b8a844
jay <153961067+jasmine-gallegos@users.noreply.github.com>
2026-04-04 17:53:26 -0500

    dear god

--- localStoreSearchWebsite/homePage/homePage.js
+++ localStoreSearchWebsite/homePage/homePage.js
@@ -1,7 +1,24 @@
+import pg from "pg";
 
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "softwareprojects2026",
+    password: "sdU*ns7Bs$",
+    port: 5432,
+});
 
-const fs = require("fs");
+db.connect();
 
+db.query("SELECT * FROM users", (err, res) => {
+    if (err) {
+        console.error("Error executing query", err.stack);
+    } else {
+        quiz = res.rows;
+    }
+
+    db.end();
+});
 
 
 // check if user is logged in or not
@@ -9,21 +26,21 @@ const fs = require("fs");
 
 
 // add event listener for the search button
-document.getElementById("search-button").addEventListener("click", function () {
-    // get user text
-    var userSearchText = document.getElementById("search-input").value;
+// document.getElementById("search-button").addEventListener("click", function () {
+//     // get user text
+//     var userSearchText = document.getElementById("search-input").value;
     
-    // get rid of white space around it
-    userSearchText = userSearchText.trim()
+//     // get rid of white space around it
+//     userSearchText = userSearchText.trim()
     
-    // if input is < 3 chars, then reject it
-    if (userSearchText.length < 3) { 
-        alert("uh maybe type some text bro");
+//     // if input is < 3 chars, then reject it
+//     if (userSearchText.length < 3) { 
+//         alert("uh maybe type some text bro");
     
-    } else {
-        window.location.href = "../searchResults/searchResults.html"
-    }
+//     } else {
+//         window.location.href = "../searchResults/searchResults.html"
+//     }
 
     
-})
+// })
 
