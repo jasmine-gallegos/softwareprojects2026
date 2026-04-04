97cfd2b11a58abb258ab2024064fffd77b239eb2
jay <153961067+jasmine-gallegos@users.noreply.github.com>
2026-04-04 14:45:59 -0500

    OH YES i am SO awesome

--- localStoreSearchWebsite/homePage/homePage.js
+++ localStoreSearchWebsite/homePage/homePage.js
@@ -1,7 +1,7 @@
 console.log(document.getElementById("search-button").textContent);
 
-document.getElementById("search-button").addEventListener("click", handleClick)
-
-function handleClick() {
-    alert("y-you touched me");
-}
+document.getElementById("search-button").addEventListener("click", function () {
+    var text = document.getElementById("search-input").value;
+    console.log(text)
+    alert(text);
+})
