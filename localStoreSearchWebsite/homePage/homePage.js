46b63a40423cf181d69fa5e60d42e4ed4c9014fa
jay <153961067+jasmine-gallegos@users.noreply.github.com>
2026-04-04 14:26:58 -0500

    im understanding the relationship from JS to HTML, its pretty smooth

---
+++ localStoreSearchWebsite/homePage/homePage.js
@@ -0,0 +1,7 @@
+console.log(document.getElementById("search-button").textContent);
+
+document.getElementById("search-button").addEventListener("click", handleClick)
+
+function handleClick() {
+    alert("y-you touched me");
+}
